ff5eeb5773151e30fe9c443e0c359be3c8544598
Xavier <tim88776@gmail.com>
2025-02-14 22:09:56 +0800

    feat: add player class

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     dt = 0
     clock = pygame.time.Clock()
@@ -14,7 +16,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         
+        player.update(dt)
         screen.fill((0, 0, 0))
+        player.draw(screen)
+        
+        # Update the full display Surface to the screen
         pygame.display.flip()
 
         # Pause the game loop until 1/60th of a sec passed
